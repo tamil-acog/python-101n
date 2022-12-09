6d8b0303d93bb905ad1a885627f01dca09a300d3
Tamil Selvan <tamil@aganitha.ai>
2022-12-09 19:36:11 +0530

    Rama's Task Manager

--- commands.py
+++ commands.py
@@ -1,34 +1,17 @@
 import typer
-from typing import Callable
-import yaml
+from atk_training_tamil_p1.task_manager import TaskManager
 
-from atk_training_tamil_p1.function_lookup import FunctionalLoader
-
-
-get = FunctionalLoader()
 app = typer.Typer()
 
 
-def apply_process_function(input_file: str, output_file: str = None) -> None:
+def apply_process_pipeline(input_file: str, output_file: str = None) -> None:
+    """This function transforms the input file using the pipeline found in yaml file."""
+    my_task_manager = TaskManager("./config.yml")
     with open(input_file, 'r') as fp, open(output_file, 'w') as op:
         lines = [line.rstrip() for line in fp]
         for line in lines:
-            transformed_line = yaml_config(line)
+            transformed_line = my_task_manager.execute_pipeline(line)
             print(transformed_line, file=op)
-
-
-def yaml_config(line: str) -> str:
-    processed_line: str = None
-    with open("config.yml", 'r') as lines:
-        tasks = yaml.safe_load(lines)
-    for i in range(len(tasks['tasks'])):
-        print(tasks['tasks'][i])
-        process_fun: Callable[[str], str] = get.get_function(tasks['tasks'][i])
-        if i == 0:
-            processed_line = process_fun(line)
-        else:
-            processed_line = process_fun(processed_line)
-    return processed_line
 
 
 @app.command()
@@ -36,8 +19,12 @@ def process_text(input_file: str, output_file: str = None) -> None:
     if output_file is None:
         output_file = input_file + ".processed"
 
-    apply_process_function(input_file, output_file)
+    apply_process_pipeline(input_file, output_file)
 
 
 def main():
+    app()
+
+
+if __name__ == "__main__":
     app()
---
+++ task_manager.py
@@ -0,0 +1,27 @@
+from typing import List, Callable
+
+import yaml
+
+from atk_training_tamil_p1.function_lookup import FunctionalLoader
+
+load = FunctionalLoader()
+
+
+class TaskManager(object):
+    def __init__(self, yaml_file: str):
+        self.task_list: List[Callable[[str], str]] = TaskManager.parse_yaml(yaml_file)
+
+    def execute_pipeline(self, line: str):
+        for task in self.task_list:
+            line = task(line)
+        return line
+
+    @classmethod
+    def parse_yaml(cls, yaml_file):
+        with open(yaml_file) as fp:
+            yaml_data = yaml.safe_load(fp)
+        task_names = yaml_data["pipeline"]
+        tasks = []
+        for task_name in task_names:
+            tasks.append(load.get_function(task_name))
+        return tasks
