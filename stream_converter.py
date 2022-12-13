b8c753285b411fbc6054b3453dc44480b1223ad1
Tamil Selvan <tamil@aganitha.ai>
2022-12-13 13:40:00 +0530

    updated yield to not to store the input file

--- stream_converter.py
+++ stream_converter.py
@@ -1,15 +1,14 @@
 
 
 class PreProcessor:
-    def process(self, input_file: str) -> str:
-        with open(input_file, 'r+') as fp, open("temp", 'w') as op:
-            lines = fp.readlines()
-            temp = self.stream_input(lines)
-            return temp
-
     @staticmethod
-    def stream_input(stream: str):
-        for lin in stream:
-            yield lin
+    def process(input_file: str) -> str:
+        with open(input_file, 'r+') as fp:
+            while True:
+                line = fp.readline()
+                yield line
+                if line == '':
+                    return
+
 
 
